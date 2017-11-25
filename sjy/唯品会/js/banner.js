81d7fe9fa3ada7ac71981894c98d14f66895cfac
Songjieyu <sjyjsnt@163.com>
2017-11-25 10:50:07 +0800

    页面复制第一次提交

---
+++ sjy/唯品会/js/banner.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+	loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect : 'fade',
+	fade: {
+  	crossFade: false,
+  	pagination : '.swiper-pagination',
+	paginationType : 'fraction',
+}
+ });
